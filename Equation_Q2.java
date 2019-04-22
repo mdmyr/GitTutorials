86ef7fb935685e9243d425455ef17e198512669a
YuvaRaj Raghunapu <mdm.yuvaraj@gmail.com>
2019-04-22 21:19:27 +0530

    staggd.

--- Equation_Q2.java
+++ Equation_Q2.java
@@ -20,7 +20,7 @@ public class Equation_Q {
 		int b = 2;
 		int n = 10;
 		/*
-		 * looping n times
+		 * loopings  n times
 		 */
 
 		int[] wholeComp;
@@ -51,7 +51,7 @@ public class Equation_Q {
 	private static int seriesComp(int b, int j) {
 		// power Caller validation
 		if (j > 0) {
-			System.out.println("Power Ca ller");
+			System.out.println("Power  Ca ller Printer");
 			return powerOf2(j) * b;
 			
 		} else
